57eb9bdeec26fade785b1c2c6b2bb8ce319edd39
KnoX <ahora.masuod.m9061@gmail.com>
2026-06-27 15:11:38 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,15 +2,23 @@
 // Service Worker – Dillimore Projects
 // ================================================================
 
-const CACHE_NAME = 'dillimore-v1';
+const CACHE_NAME = 'dillimore-v2';
 const urlsToCache = [
   '/',
   '/StarWars.html',
   '/Snake.html',
   '/Calculator.html',
   '/Calendar.html',
+  '/TicTacToe.html',
   '/404.html',
-  'https://uploadkon.ir/uploads/0bd526_26InShot-20250821-043319341.png'
+  '/manifest.json',
+  '/sw.js',
+  'https://uploadkon.ir/uploads/0bd526_26InShot-20250821-043319341.png',
+  'https://uploadkon.ir/uploads/d5c326_26download.png',
+  'https://uploadkon.ir/uploads/292a26_26calculator-icon-free-vector.jpg',
+  'https://uploadkon.ir/uploads/bf2b26_26download.png',
+  'https://uploadkon.ir/uploads/2ad227_26download.png',
+  'https://uploadkon.ir/uploads/966926_264155897.png'
 ];
 
 // Install Service Worker
